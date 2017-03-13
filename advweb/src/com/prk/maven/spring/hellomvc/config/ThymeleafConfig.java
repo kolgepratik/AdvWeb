35f6c4c41f4127695150583950aeb5a5cceebb98
Curtis McDonald <curtis.mcdonald@hmitsolutions.com>
2017-03-12 21:45:27 -0400

    Added Login pages and controller

--- advweb/src/com/prk/maven/spring/hellomvc/config/ThymeleafConfig.java
+++ advweb/src/com/prk/maven/spring/hellomvc/config/ThymeleafConfig.java
@@ -3,14 +3,17 @@
  */
 package com.prk.maven.spring.hellomvc.config;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
@@ -34,12 +37,25 @@ public class ThymeleafConfig extends WebMvcConfigurerAdapter implements Applicat
 	 */
 
 	private ApplicationContext applicationContext;
+	
+	public ThymeleafConfig() {
+        super();
+    }
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
-
+	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+	
+	/**
+     *  Message externalization/internationalization
+     */
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("Messages");
+        return resourceBundleMessageSource;
+    }
+	
 	@Bean
 	public ITemplateResolver templateResolver() {
 		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
@@ -55,6 +71,7 @@ public class ThymeleafConfig extends WebMvcConfigurerAdapter implements Applicat
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.addDialect(new LayoutDialect());
 		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.addDialect(new SpringSecurityDialect());
 
 		return templateEngine;
 	}
